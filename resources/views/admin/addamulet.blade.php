70927b6de31a8890156cc4bd88e0703ee4422a3d
charamp <parinya.peapanom@gmail.com>
2017-01-12 21:51:44 +0700

    modify addamulet function

--- resources/views/admin/addamulet.blade.php
+++ resources/views/admin/addamulet.blade.php
@@ -12,15 +12,47 @@
     <form class="col s12">
       	<div class="row addamulet-card">
 	        <div class="input-field col s12">
-	          	<input placeholder="ชื่อพระเครื่อง" id="amulet_name" type="text" class="validate">
-	          	<label for="first_name">ชื่อพระเครื่อง</label>
+	          	<input id="amulet_name" type="text" class="validate" length='255'>
+	          	<label for="amulet_name">ชื่อพระเครื่อง</label>
 	        </div>
       	</div>
       	<div class="row addamulet-card">
 	        <div class="input-field col s12">
-	          	<textarea placeholder="รายละเอียดพระเครื่อง" id="amulet_description" type="text" class="materialize-textarea"></textarea>
-	          	<label for="first_name">รายละเอียดพระเครื่อง</label>
+	          	<textarea id="amulet_description" type="text" class="materialize-textarea" length='500'></textarea>
+	          	<label for="amulet_description">รายละเอียดพระเครื่อง</label>
 	        </div>
+      	</div>
+      	<div class="row addamulet-card">
+	        <div class="input-field col s12">
+	          	<input id="amulet_author" type="text" class="validate" length='255'>
+	          	<label for="amulet_author">พระเกจิผู้สร้าง</label>
+	        </div>
+      	</div>
+      	<div class="row addamulet-card">
+	        <div class="input-field col s6">
+	          	<input id="amulet_price" type="text" class="validate" length='10'>
+	          	<label for="amulet_price">ราคาพระเครื่อง</label>
+	        </div>
+	        <div class="input-field col s6">
+			    <select>
+			      	<option value="พร้อมเช่า" selected>พร้อมเช่า</option>
+			      	<option value="เช่าแล้ว">เช่าแล้ว</option>
+			      	<option value="พระโชว์">พระโชว์</option>
+			    </select>
+			    <label>สถานะพระเครื่อง</label>
+			</div>
+      	</div>
+      	<div class='row addamulet-card add-image'>
+			<div class="file-field input-field">
+				<div class="btn">
+					<span>รูปพระ</span>
+					<input type="file">
+				</div>
+				<div class="file-path-wrapper">
+					<input class="file-path validate" type="text">
+				</div>
+			</div>
+
       	</div>
     </form>
   </div>
