679acf30470d07396b296f51667d2b6fd50ef3fb
charamp <parinya.peapanom@gmail.com>
2017-01-14 10:59:37 +0700

    modify load amulet ajax function

--- public/js/boonya-script.js
+++ public/js/boonya-script.js
@@ -1,3 +1,41 @@
+
+var AmuletList = (function () {
+	'use strict';
+
+	append: function (data) {
+      var rowElement = createRowElement(data);
+      $containerElement.append(rowElement);
+      filter(rowElement);
+    },
+});
+
+var AmuletManager = (function (AmuletList) {
+	'use strict';
+	var loading = false;
+
+	function loadAmulet(category_id) {
+		if(loading) return;
+		loading = true;
+
+	    $.get('/loadamuletcategory', { category_id : category_id }, function (data) {
+	      var context = {};
+	      runner(context, function () {
+	        AmuletList.append(data.shift());
+	        context.finished = !data.length;
+	      });
+	      loading = false;
+	    });
+	}
+});
+
+
 $(document).ready(function() {
-    $('select').material_select();
-  });
+	$('select').material_select();
+
+	$('.collection-item').click(function() {
+		$('.collection-item').removeClass('active');
+		$(this).addClass('active');
+		console.log($(this).attr('id'));
+		loadAmulet($(this).attr('id'));
+	});
+});
